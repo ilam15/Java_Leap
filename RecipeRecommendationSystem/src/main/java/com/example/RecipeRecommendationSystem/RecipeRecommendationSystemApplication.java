a4408fb5ef58640b33b158923aebee635397a12c
ilam15 <ilamsaravan@gmail.com>
2025-10-29 09:34:21 +0530

    Spring Boot Template Added

---
+++ RecipeRecommendationSystem/src/main/java/com/example/RecipeRecommendationSystem/RecipeRecommendationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.RecipeRecommendationSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeRecommendationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeRecommendationSystemApplication.class, args);
+	}
+
+}
---
+++ RecipeRecommendationSystem/src/test/java/com/example/RecipeRecommendationSystem/RecipeRecommendationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RecipeRecommendationSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeRecommendationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
